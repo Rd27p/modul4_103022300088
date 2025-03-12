8f228fe7bc96f9760d7ead41b3da02fa212a4be3
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 16:14:14 +0700

    menambahkan class FanLaptop

---
+++ FanLaptop.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_103022300088
+{
+    internal class FanLaptop
+    {
+        public enum FanState
+        {
+            Quiet, Balanced, Performance, Turbo
+        };
+        public enum Trigger
+        {
+            Mode_Down, Mode_Up, Turbo_Shortcut
+        };
+
+        public class Transition
+        {
+            public FanState PrevState { get; }
+            public FanState NextState { get; }
+            public Trigger Trigger { get; }
+
+            public Transition(FanState prevState, FanState nextState, Trigger trigger)
+            {
+                PrevState = prevState;
+                NextState = nextState;
+                Trigger = trigger;
+            }
+        }
+
+        private Transition[] transitions =
+        {
+            new Transition(FanState.Quiet, FanState.Balanced, Trigger.Mode_Up),
+            new Transition(FanState.Balanced, FanState.Quiet, Trigger.Mode_Down),
+            new Transition(FanState.Balanced, FanState.Performance, Trigger.Mode_Up),
+            new Transition(FanState.Performance, FanState.Balanced, Trigger.Mode_Down),
+            new Transition(FanState.Performance, FanState.Turbo, Trigger.Mode_Up),
+            new Transition(FanState.Turbo, FanState.Performance, Trigger.Mode_Down),
+            new Transition(FanState.Quiet, FanState.Turbo, Trigger.Turbo_Shortcut),
+            new Transition(FanState.Turbo, FanState.Quiet, Trigger.Turbo_Shortcut),
+        };
+
+        public FanState CurrentState { get; private set; } = FanState.Quiet;
+
+        public FanState GetNextState(FanState prevState , Trigger trigger)
+        {
+            foreach (var transition in transitions)
+            {
+                if (transition.PrevState == prevState && transition.Trigger == trigger)
+                {
+                    return transition.NextState;
+                }
+            }
+            return prevState;
+        }
+
+        public void ActivateState(Trigger trigger)
+        {
+            CurrentState = GetNextState(CurrentState, trigger);
+        }
+    }
+}       
--- Program.cs
+++ Program.cs
@@ -15,8 +15,27 @@ namespace modul4_103022300088
             foreach (KodeProduk.Produk produk in Enum.GetValues(typeof(KodeProduk.Produk)))
             {
                 string namaProduk = produk.ToString();
-                Console.WriteLine($"{ namaProduk,-14} { KodeProduk.getKodeProduk(produk)}");
+                Console.WriteLine($"{ namaProduk,-18} { KodeProduk.getKodeProduk(produk)}");
             }
+
+            FanLaptop fan = new FanLaptop();
+            Console.WriteLine("\nSimulasi Perubahan Fan Laptop");
+            fan.ActivateState(FanLaptop.Trigger.Mode_Up);
+            Console.WriteLine($"Fan Laptop saat ini: {fan.CurrentState}");
+            fan.ActivateState(FanLaptop.Trigger.Mode_Up);
+            Console.WriteLine($"Fan Laptop saat ini: {fan.CurrentState}");
+            fan.ActivateState(FanLaptop.Trigger.Mode_Down);
+            Console.WriteLine($"Fan Laptop saat ini: {fan.CurrentState}");
+            fan.ActivateState(FanLaptop.Trigger.Mode_Down);
+            Console.WriteLine($"Fan Laptop saat ini: {fan.CurrentState}");
+            fan.ActivateState(FanLaptop.Trigger.Turbo_Shortcut);
+            Console.WriteLine($"Fan Laptop saat ini: {fan.CurrentState}");
+            fan.ActivateState(FanLaptop.Trigger.Mode_Down);
+            Console.WriteLine($"Fan Laptop saat ini: {fan.CurrentState}");
+            fan.ActivateState(FanLaptop.Trigger.Mode_Up);
+            Console.WriteLine($"Fan Laptop saat ini: {fan.CurrentState}");
+            fan.ActivateState(FanLaptop.Trigger.Turbo_Shortcut);
+            Console.WriteLine($"Fan Laptop saat ini: {fan.CurrentState}");
         }
     }
 }
